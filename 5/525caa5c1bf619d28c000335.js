10afe183e7d319d3fa494c460e8db46bed4903b6
Evgeniia <Kochetkovaev11@gmail.com>
2023-03-10 11:16:44 +0300

    08.03.2023

--- 5/525caa5c1bf619d28c000335.js
+++ 5/525caa5c1bf619d28c000335.js
@@ -30,7 +30,12 @@ console.log(
   ])
 );
 
-//012 + 1
-//036 + 1
-//048
-//246
+//Best Solutions
+
+function isSolved(board) {
+  board = board.join("-").replace(/,/g, "");
+  if (/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
+  if (/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
+  if (/0/.test(board)) return -1;
+  return 0;
+}
